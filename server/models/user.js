21297d44037ecea8ab831c765c10ba282054d561
Vladimir Vladimirov <v.vladimirov@deltastock.com>
2018-04-16 16:33:20 +0300

    Test POST /todos route

--- server/models/user.js
+++ server/models/user.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var User = mongoose.model('User', {
     email: {
         type: String,
-        require: true,
+        required: true,
         minlength: 1,
         trim: true
     }
