d67464330b5fc1f233bc4661199729a89f586a3d
Vladimir Vladimirov <v.vladimirov@deltastock.com>
2018-04-24 14:37:28 +0300

    Update config to use JSON file

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -12,7 +12,7 @@ const users = [{
     password: 'userOnePass',
     tokens: [{
         access: 'auth',
-        token: jwt.sign({_id: userOneId, access: 'austh'}, 'abc123').toString()
+        token: jwt.sign({_id: userOneId, access: 'austh'}, process.env.JWT_SECRET).toString()
     }]
 }, {
     _id: userTwoId,
@@ -20,7 +20,7 @@ const users = [{
     password: 'userTwoPass',
     tokens: [{
         access: 'auth',
-        token: jwt.sign({_id: userTwoId, access: 'austh'}, 'abc123').toString()
+        token: jwt.sign({_id: userTwoId, access: 'austh'}, process.env.JWT_SECRET).toString()
     }]
 }];
 
