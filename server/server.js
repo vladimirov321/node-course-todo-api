b687930721679e0b5b2aec26d5cd31207c9f8a3f
Vladimir Vladimirov <v.vladimirov@deltastock.com>
2018-04-24 10:25:36 +0300

    Add tests for POST /users/login

--- server/server.js
+++ server/server.js
@@ -93,7 +93,6 @@ app.patch('/todos/:id', (req, res) => {
   });
 });
 
-// POST /users
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
@@ -107,7 +106,6 @@ app.post('/users', (req, res) => {
   });
 });
 
-// POST /users/login {email, password}
 app.post('/users/login', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   
