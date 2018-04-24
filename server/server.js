8a6f7bacdc109716badbd48ce3f1194059dfd5ca
Vladimir Vladimirov <v.vladimirov@deltastock.com>
2018-04-24 10:57:28 +0300

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -106,6 +106,10 @@ app.post('/users', (req, res) => {
   });
 });
 
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+});
+
 app.post('/users/login', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   
@@ -118,8 +122,12 @@ app.post('/users/login', (req, res) => {
   });
 });
 
-app.get('/users/me', authenticate, (req, res) => {
-  res.send(req.user);
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
 });
 
 app.listen(port, () => {
